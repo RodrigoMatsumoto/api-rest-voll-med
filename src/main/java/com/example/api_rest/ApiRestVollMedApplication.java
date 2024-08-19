99153fb525c838bf8c75a10a381d0a24027a79a1
Rodrigo Matsumoto <matsumoto.rodrigo1@gmail.com>
2024-08-19 19:51:27 -0300

    criação do projeto

---
+++ src/main/java/com/example/api_rest/ApiRestVollMedApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestVollMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestVollMedApplication.class, args);
+	}
+
+}
